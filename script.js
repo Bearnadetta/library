a33701c746dbcba573bb272fed90c8efc2c37dfb
Bearnadetta <bearnadetta@gmail.com>
2021-08-28 14:34:57 -0700

    added basic HTML, js, and css

---
+++ script.js
@@ -0,0 +1,9 @@
+let myLibrary = [];
+
+function Book() {
+    //constructor function for book objects
+}
+
+function addBookToLibrary() {
+    //adds the book from the form section to the library
+}
